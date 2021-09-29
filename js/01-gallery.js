88c4c273dbc0ed12e911b65e27a14962abee05d9
PawelS1988 <szatkowskipawel1988@gmail.com>
2021-09-29 12:16:48 +0200

    done(i think)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("div.gallery");
+const createGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} data-source=${original} height="200">
+  </a>
+  </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", createGallery);
+
+gallery.addEventListener("click", selectPict);
+
+function selectPict(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  galleryItems.forEach((item) => {
+    const modalWindow = basicLightbox.create(`<img src=${item.original}>`);
+    if (event.target.src === item.preview) {
+      modalWindow.show();
+    }
+    if (modalWindow.visible() === true) {
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+          modalWindow.close();
+        }
+      });
+    }
+  });
+}
